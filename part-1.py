62391e38b5aebc69a08bb783c42005180f6d36c5
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-05-05 20:34:46 -0700

    implements part1 problem sets

--- part-1.py
+++ part-1.py
@@ -4,17 +4,35 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n == 0:
+        return 1
+    if n < 1:
+        raise ValueError
+    return n * factorial(n-1)
 
 
 # reverse
-
+def reverse(text):
+    if text == "":
+        return ""
+    return text[-1] + reverse(text[:-1])
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return 0
+    return 2 + bunny(count-1)
 
 
 # is_nested_parens
-
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+    if len(parens) % 2 != 0:
+        return False
+    if parens[0] == "(" and parens[-1] == ")":
+        return is_nested_parens(parens[1:-1])
+    return False
 
